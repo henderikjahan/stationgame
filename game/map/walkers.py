8ed0388da0936dc61009e48492ccfbf70511ae6f
henderikjahan <momojohobo@rocketship.com>
2022-09-06 09:34:42 +0200

    map traversal: basic lighting

--- game/map/walkers.py
+++ game/map/walkers.py
@@ -1,4 +1,5 @@
 from panda3d.core import NodePath, Vec2, Vec3
+from panda3d.core import PointLight
 
 from .common import DIRS, OPPO
 from game.tools import roundvec
@@ -16,7 +17,7 @@ class TileWalker:
     def forward(self, duration=0.3):
         x, y = DIRS["nesw"[self.direction]]
         start_pos = roundvec(self.root.get_pos())
-        next_pos = Vec3(start_pos.x+x,start_pos.y+y, 0)         
+        next_pos = Vec3(start_pos.x+x,start_pos.y+y, 0)
         tile = self.tiles[next_pos.x, -next_pos.y]
         if not tile.char == "#":
             return self.root.posInterval(duration, next_pos, startPos=start_pos)
@@ -25,7 +26,7 @@ class TileWalker:
         self.direction += a
         self.direction %= 4
         start_hpr = roundvec(self.root.get_hpr(render))
-        next_hpr = start_hpr+Vec3(a*-90, 0, 0) 
+        next_hpr = start_hpr+Vec3(a*-90, 0, 0)
         return self.root.quatInterval(duration, next_hpr, startHpr=start_hpr, blendType='easeOut')
 
 
@@ -37,7 +38,10 @@ class CameraWalker(TileWalker):
         base.camLens.set_near(0.1)
         base.camLens.set_fov(90)
         base.task_mgr.add(self.update)
-        
+        self.light = self.root.attach_new_node(PointLight("walker"))
+        self.light.node().set_attenuation(Vec3(0.1,0.1,0.1))
+        render.set_light(self.light)
+
     def update(self, task):
         if base.sequencer.running:
             return task.cont
@@ -48,5 +52,5 @@ class CameraWalker(TileWalker):
             base.sequencer.add(self.turn())
         elif context["turnleft"]:
             base.sequencer.add(self.turn(-1))
-            
+
         return task.cont
